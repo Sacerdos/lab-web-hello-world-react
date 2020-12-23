03e06abdcc418ecd5ed06724caed5ad09d2d2aa6
Ilya Dychkov <ilya.dychkov@gmail.com>
2020-12-24 01:19:01 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './1359974.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World от Дычкова Ильи ИКБО-04-17
+
+        </p>
+        <p>Учащийся в <a
+          className="App-link"
+          href="https://www.mirea.ru/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          МИРЭА
+      </a></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
